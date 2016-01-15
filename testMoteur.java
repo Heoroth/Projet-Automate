817b2918ad10b0db790efa042f3aff3249967652
Heoroth <rusiecki.romain@gmail.com>
2016-01-15 13:45:37 +0100

    Create testMoteur.java

---
+++ testMoteur.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+
+public class testMoteur {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Moteur monMoteur = new Moteur();
+		monMoteur.afficheAlphabetEntree();
+		monMoteur.afficheNombreEtat();
+		monMoteur.afficheEtatInit();
+	}
+
+}
